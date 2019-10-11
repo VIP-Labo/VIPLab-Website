8fbcea8fbd94c723010b1166ca2896236ff3eece
VIP-Lab-web <56115941+VIP-Lab-web@users.noreply.github.com>
2019-10-11 14:58:11 +0900

    2019.ver websitecode

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//jQuery init
+
+$(document).ready(function(){
+	// sidenavの設定
+
+  //ウィンドウの高さを取得
+  var windowHeight = $(window).height() -64;
+
+	$('.sidenav').sidenav({
+		edge:'right',
+	});
+
+  	// sidenav-li がクリックされたときにsidenavを閉じる
+  	$('.sidenav-li').click(function(){
+  		$('.sidenav').sidenav('close');
+  	});
+
+     //トップまでのスクロール
+     $('.scroll-btn').click(function(){
+     	$("html,body").animate({scrollTop:0});
+     });
+
+	// モーダルの設定
+	$('.modal').modal();
+
+	// スライダーの設定
+  $('.slider').slider({
+    height: windowHeight,
+    indicators: false
+  });
+
+   // ドロップダウンの処理
+   $(".dropdown-trigger").dropdown();
+
+      // #で始まるアンカーをクリックした場合に処理
+      $('a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+
+      // スムーススクロール(navの分引く)
+      $('body,html').animate({scrollTop:position-64}, speed, 'swing');
+
+      return false;
+  });
+
+});
